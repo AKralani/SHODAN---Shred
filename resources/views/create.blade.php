2037b89fd14e0c52cd26f788c5bbfdfc0714f935
ingridafeimi <50929817+ingridafeimi@users.noreply.github.com>
2020-06-30 11:11:12 +0200

    Revert "created views"

--- resources/views/create.blade.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    /**
-     * Kjo faqe do te permbaje formen per te krijuar nje postim te ri.
-     */
--- resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    /*
-     * Kjo eshte faqja e pare qe do te permbaje edhe listen e postimeve sipas nje kategorie te caktuar.
-     * */
