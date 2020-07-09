698686ffe9370f2ca2069e2a76f241fc5cf00773
Rron Aliu <rronaliu55@gmail.com>
2020-07-09 14:06:32 +0200

    e nderqa edhe pak profile page. ProfilePageBugsFix2 dmth kjo po nuk bona new branch

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -4,10 +4,18 @@
 <div class="container">
 <div class="row justify-content-center">
     <div class="col-md-12">
-        <div class="card" >
-            <div class="card-header">
-            <h1>Profile page for {{ $user->name }}</h1>
-            <p>Joined at {{ $user->created_at->diffForHumans() }}</p>
+        <div class="card">
+            <div class="card-header" class="inline-text">
+            <img src="https://i.pravatar.cc/150?u=fake@pravatar.com"
+                  alt=""
+                  class="m-2 rounded-circle mr-2 absolute bottom-0"
+                  style="float:left"
+                  width="150"
+            >
+                <div class="header p-5" style="float:left">
+                    <h1>Profile page for {{ $user->name }}</h1>
+                    <p>Joined at {{ $user->created_at->diffForHumans() }}</p>
+                </div>
             </div>
             <div class="card-body">
                  @include('_timeline', [
