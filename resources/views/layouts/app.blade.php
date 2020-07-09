da053cd39069be65ded05e8b4f0db9fc3606d2c1
AKralani <56108617+AKralani@users.noreply.github.com>
2020-07-09 14:56:13 +0200

    Change Users button
    Signed-off-by: AKralani <56108617+AKralani@users.noreply.github.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
                 </a>
                 <a class="navbar-brand" href="{{ url('/search-users') }}">
                     <!-- {{ config('app.name', 'Laravel') }} -->
-                    Search button
+                    Users
                  </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
