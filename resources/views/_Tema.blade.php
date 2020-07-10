b0ecd2e5414d41faf58ac51ab2920de7d3114a65
Rron Aliu <rronaliu55@gmail.com>
2020-07-10 15:16:11 +0200

    added link prej Profile card ne user profile

--- resources/views/_Tema.blade.php
+++ resources/views/_Tema.blade.php
@@ -1,21 +1,5 @@
-<!-- <div class="card-header inline-text">Tema</div>
-
-<div class="card-body">
-        <a href="#" class="text-decoration-none list-group-item list-group-item-action list-group-item-light">
-        <img src="https://via.placeholder.com/150"
-                  alt=""
-                  class="rounded-circle m-2 absolute bottom-0"
-                  style="float:left"
-                  width="40"
-            >
-        </a></li>
-        <a href="#" class="text-decoration-none list-group-item list-group-item-action list-group-item-light">Profile</a></li>
-    </ul>
-</div> 
- -->
-
 <!-- Card -->
-<a href="#" class="text-reset text-decoration-none">
+<a href="{{ route('profile', auth()->user()->name) }}" class="text-decoration-none text-dark">
 <div class="card text-center text-grey shadow p-3" style="background:#a4a4a4">
 
   <!-- Card content -->
