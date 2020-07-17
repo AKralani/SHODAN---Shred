a50824b48aea82f71d5e9ef8ebe30619dad26446
ermiravuciterna <63071794+ermiravuciterna@users.noreply.github.com>
2020-07-17 15:06:28 +0200

    Edit profile-only about you isnt working

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,6 +13,34 @@ class ProfileController extends Controller
         return view('profiles.show', compact('user'));
     }
 
+    public function edit(User $user)
+    {
+        return view('profiles.edit', compact('user'));
+    }
+    
+    public function update(User $user)
+    {
+        $attributes = request()->validate([
+
+            'name'=> ['string', 'required', 'max:255'],
+            'avatar'=>['file'],
+            'email'=> ['string', 'required', 'email', 'max:255'],
+            'password'=>['string', 'required', 'confirmed']
+        ]);
+
+        if(request('avatar')){
+
+        $attributes['avatar'] = request('avatar')->store('avatars');
+
+        }
+
+        $user->update($attributes);
+
+        return redirect($user->path());
+    }
+
+
+
     
 }
 
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -6,16 +6,21 @@
     <div class="col-md-12">
         <div class="card bg-dark text-light shadow-lg">
             <div class="card-header bg-dark shadow" class="inline-text">
-            <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg"
+            <img src="/uploads/avatar/{{ $user->avatar }}"
                   alt=""
                   class="m-2 rounded-circle mr-2 absolute bottom-0"
                   style="float:left"
                   width="150"
             >
-                <div class="header p-5" style="float:left">
-                    <h1>Profile page for {{ $user->name }}</h1>
-                    <h3>Lorem ipsum some bio or whatever</h3>
+                <div class="header p-5" style="float:left" >
+                    <h1>{{ $user->name }}</h1>
+                    <h3>My profile page{{ $user->about }}</h3>
                     <p>Joined at {{ $user->created_at->diffForHumans() }}</p>
+                <div class="flex">
+                    @can ('edit', $user)
+                    <a href="{{ $user->path('edit') }}" class="rounded-full border border-gray-300 py-2 px-2 text-white text-xs mr-2">Edit profile</a>
+                    @endcan
+                </div>    
                 </div>
             </div>
             <div class="card-body" style="background:#a4a4a4">
