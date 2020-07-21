4a6f0e4e88c81e01661fa69b62b6d59f5307ba45
Ingrida Feimi <ingridafeimi@gmail.com>
2020-07-21 07:56:41 +0200

    shtova imazhin tek lista e postimeve

--- resources/views/_post.blade.php
+++ resources/views/_post.blade.php
@@ -1,48 +1,56 @@
 <div>
     <div class="card text-white bg-dark mb-3 shadow">
-    <a href="{{ route('profile', $post->user) }}" class="text-decoration-none" style="color:white">
-    <div class="card-header inline-text bg-dark">
-        <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg"
-                  alt=""
-                  class="rounded-circle absolute bottom-0"
-                  style="float:left"
-                  width="50"
-            >
-            <h2 style="float:left" class="m-2 px-2">{{ $post->user->name}}</h2>
-            <p class="m-3 text-light float-right"> {{ $post->created_at->diffForHumans() }}</p>
-        </a>
+        <div class="card-header inline-text bg-dark">
+            <a href="{{ route('profile', $post->user) }}" class="text-decoration-none" style="color:white">
+                <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg"
+                     alt=""
+                     class="rounded-circle absolute bottom-0"
+                     style="float:left"
+                     width="50"
+                >
+                <h2 style="float:left" class="m-2 px-2">{{ $post->user->name}}</h2>
+                <p class="m-3 text-light float-right"> {{ $post->created_at->diffForHumans() }}</p>
+            </a>
         </div>
 
         <a href="{{ route('post.show', $post->id ) }}" class="text-decoration-none">
-        <div class="card-body text-dark" style="background:#a4a4a4">
-             <div class="card-title"><h3>{{ $post->title }}</h3></div>
-            <p class="card-text">{{ $post->body }}</p>
-        </div>
+            <div class="card-body text-dark" style="background:#a4a4a4">
+
+                @if($post->image)
+                    <img class="mb-2 rounded" src="{{ $post->image  }}" style="width: 200px; height: 200px; object-fit: cover;"/>
+                @endif
+
+                <div class="card-title"><h3>{{ $post->title }}</h3></div>
+                <p class="card-text">{{ $post->body }}</p>
+            </div>
         </a>
 
         <div class="form rounded-bottom" class="inline-text" style="background:#a4a4a4">
-        
-        <hr>
-        
-        <form action="{{ route('posts.destroy', $post->id)  }}" method="post">
-            @csrf
-            @method('DELETE')
-            <input type="submit"  name="submit"  value="Delete" class="btn btn-danger m-2 px-3" style= "float:right" >
-            <button type="button" class="btn btn-primary m-2 px-4 float-right" data-toggle="modal" data-target="#edit" data-id="{{ $post->id }}" data-title="{{ $post->title }}" data-body="{{ $post->body }}">Edit</button>
-        </form>
 
-        <div class="inline-text">
-        <!-- <p class="m-3 text-dark float-left"> {{ $post->created_at->diffForHumans() }}</p> -->
-        <p class="m-3 text-dark float-left"> {{count($post->comments) }} Comments</p>
-        
-        </div>
+            <hr>
+
+            <form action="{{ route('posts.destroy', $post->id)  }}" method="post">
+                @csrf
+                @method('DELETE')
+                <input type="submit" name="submit" value="Delete" class="btn btn-danger m-2 px-3" style="float:right">
+                <button type="button" class="btn btn-primary m-2 px-4 float-right" data-toggle="modal"
+                        data-target="#edit" data-id="{{ $post->id }}" data-title="{{ $post->title }}"
+                        data-body="{{ $post->body }}">Edit
+                </button>
+            </form>
+
+            <div class="inline-text">
+            <!-- <p class="m-3 text-dark float-left"> {{ $post->created_at->diffForHumans() }}</p> -->
+                <p class="m-3 text-dark float-left"> {{count($post->comments) }} Comments</p>
+
+            </div>
 
 
-        @auth
-        <x-like-buttons :post="$post" />
-        @endauth
+            @auth
+                <x-like-buttons :post="$post"/>
+            @endauth
 
-        <p class="m-3 text-dark"> {{ $post->created_at->diffInHours() }} Hours ago</p>
+            <p class="m-3 text-dark"> {{ $post->created_at->diffInHours() }} Hours ago</p>
 
         </div>
     </div>
